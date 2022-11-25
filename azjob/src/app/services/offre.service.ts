890b2c081b29aa09ea5a920b858ec37770618ab5
afafbentouil <afafbentouil@outlook.fr>
2022-11-25 22:42:40 +0100

    service Offre

--- azjob/src/app/services/offre.service.ts
+++ azjob/src/app/services/offre.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Offre } from '../models/offre.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OffreService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllOffre() {
+    return this.http.get('http://localhost:8080/offres/');
+  }
+
+  getOffreById(id: any) {
+    return this.http.get('http://localhost:8080/offres/' + id);
+  }
+
+  createOffre(offre: Offre) {
+    return this.http.post('http://localhost:8080/offres/', offre);
+  }
+
+  updateOffre(id: any, offre: Offre) {
+    return this.http.put('http://localhost:8080/offres/' + id, offre);
+  }
+
+  deleteOffre(id: any) {
+    return this.http.delete('http://localhost:8080/offres/' + id);
+  }
 }
