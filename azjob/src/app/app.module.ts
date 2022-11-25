81091bfa5fc1699cd04b7b81a1c81ae0efe4522b
afafbentouil <afafbentouil@outlook.fr>
2022-11-25 22:12:51 +0100

    adding new components

--- azjob/src/app/app.module.ts
+++ azjob/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ListeOffresComponent } from './components/liste-offres/liste-offres.component';
+import { ListeCandidaturesComponent } from './components/liste-candidatures/liste-candidatures.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent,
+    ListeOffresComponent,
+    ListeCandidaturesComponent
   ],
   imports: [
     BrowserModule,
