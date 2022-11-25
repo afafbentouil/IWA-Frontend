a040ca7634d3c88d639307bf6dca108cb88a6fe0
afafbentouil <afafbentouil@outlook.fr>
2022-11-25 22:12:00 +0100

    liste offres

---
+++ azjob/src/app/components/liste-offres/liste-offres.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-offres',
+  templateUrl: './liste-offres.component.html',
+  styleUrls: ['./liste-offres.component.scss']
+})
+export class ListeOffresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
