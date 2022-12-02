4bd6439bd381ebce14eafb21784a9db8e1182e05
afafbentouil <afafbentouil@outlook.fr>
2022-12-02 19:50:13 +0100

    not committed files

--- azjob/src/app/app-routing.module.ts
+++ azjob/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListeCandidaturesComponent } from './components/liste-candidatures/liste-candidatures.component';
 import { ListeOffresComponent } from './components/liste-offres/liste-offres.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProfilCandidatComponent } from './components/profil-candidat/profil-candidat.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { UserComponent } from './components/user/user.component';
 
@@ -11,7 +12,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
   { path: 'user', component: UserComponent },
-  { path: 'offres', component: ListeOffresComponent }
+  { path: 'offres', component: ListeOffresComponent },
+  { path: 'profil', component: ProfilCandidatComponent }
   //{ path: '/candidatures', component: ListeCandidaturesComponent }
 ];
 
--- azjob/src/app/app.module.ts
+++ azjob/src/app/app.module.ts
@@ -13,6 +13,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 import { LocalStorageService } from './services/local-storage.service';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProfilCandidatComponent } from './components/profil-candidat/profil-candidat.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,10 @@ import { LocalStorageService } from './services/local-storage.service';
     LoginComponent,
     RegistrationComponent,
     ListeOffresComponent,
-    ListeCandidaturesComponent
+    ListeCandidaturesComponent,
+    NotFoundComponent,
+    ProfilCandidatComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
